03d97e8e41046c326fb08b67238442987f721feb
pcroland <pcroland@protonmail.ch>
2022-12-22 09:48:29 +0100

    make archived chats dynamic

--- viewer.py
+++ viewer.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 
 import toml
-from telethon.sync import TelegramClient, events
+from telethon import TelegramClient, utils, types, events
 
 config = toml.load("config.toml")
 
@@ -9,17 +9,32 @@ client = TelegramClient(config["phone_number"], config["api_id"], config["api_ha
 client.start()
 
 async def main():
-    archived = []
+    archived = set()
 
     async for d in client.iter_dialogs():
         if d.archived:
-            archived.append(d.id)
+            archived.add(d.id)
             if d.unread_count > 0:
                 await client.send_read_acknowledge(d)
 
-    @client.on(events.NewMessage(archived))
+    @client.on(events.NewMessage(func=lambda e: e.chat_id in archived))
     async def read(e):
         await e.mark_read()
+
+    @client.on(events.Raw(types.UpdateFolderPeers))
+    async def _(e):
+        added = e.folder_peers[0].folder_id == 1
+        peers = (utils.get_peer_id(x.peer) for x in e.folder_peers)
+
+        if added:
+            for i in peers:
+                print(f"chat {i} has been added to the archived list.")
+                archived.add(i)
+                await client.send_read_acknowledge(i)
+        else:
+            for i in peers:
+                print(f"chat {i} has been removed from the archived list.")
+                archived.remove(i)
 
     await client.run_until_disconnected()
 
